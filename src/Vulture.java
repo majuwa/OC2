0b50ab0c52cde9dacff36ab54cf271a7abc90af7
Maximilian Walter <maximilian.j.walter@gmail.com>
2015-05-14 14:39:14 +0200

    Initial Werte für Übung01

---
+++ src/Vulture.java
@@ -0,0 +1,61 @@
+import jnibwapi.JNIBWAPI;
+import jnibwapi.Position;
+import jnibwapi.Unit;
+import jnibwapi.types.WeaponType;
+
+import java.util.HashSet;
+
+public class Vulture {
+
+    final private JNIBWAPI bwapi;
+    private final HashSet<Unit> enemyUnits;
+    final private Unit unit;
+
+    public Vulture(Unit unit, JNIBWAPI bwapi, HashSet<Unit> enemyUnits) {
+        this.unit = unit;
+        this.bwapi = bwapi;
+        this.enemyUnits = enemyUnits;
+    }
+
+    public void step() {
+        /**
+         * TODO: XCS
+         */
+    	System.out.println("test");
+        Unit target = getClosestEnemy();
+        move(target);
+    }
+
+    private void move(Unit target) {
+        unit.move(new Position(target.getPosition().getPX(), target.getPosition().getPY()), false);
+    }
+
+    private Unit getClosestEnemy() {
+        Unit result = null;
+        double minDistance = Double.POSITIVE_INFINITY;
+        for (Unit enemy : enemyUnits) {
+            double distance = getDistance(enemy);
+            if (distance < minDistance) {
+                minDistance = distance;
+                result = enemy;
+            }
+        }
+
+        return result;
+    }
+
+    private double getDistance(Unit enemy) {
+        int myX = unit.getPosition().getPX();
+        int myY = unit.getPosition().getPY();
+
+        int enemyX = enemy.getPosition().getPX();
+        int enemyY = enemy.getPosition().getPY();
+
+        int diffX = myX - enemyX;
+        int diffY = myY - enemyY;
+
+        double result = Math.pow(diffX, 2) + Math.pow(diffY, 2);
+
+        return Math.sqrt(result);
+    }
+}
